19b04a57072f5c4fcffeaf2c1cb67e4722839b1f
Kalhara Batangala <kalharabatangala@gmail.com>
2025-09-05 20:49:15 +0530

    updated as python package and readme

--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,4 @@
-from app import add, subtract, multiply
+from hello_world.app import add, subtract, multiply
 
 def test_add():
     assert add(4,3) == 7
@@ -7,5 +7,5 @@ def test_subtract():
     assert subtract(4,3) == 1
 
 def test_multiply():
-    assert multiply(4,3) == 12
+    assert multiply(4,3) == 11
 
